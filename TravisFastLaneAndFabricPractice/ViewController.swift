ebf8e9e3ecbaf44ff520429e9c92c5c1337e842d
yyusheng <yyusheng@audible.com>
2018-03-29 14:11:27 -0400

    project setup

---
+++ TravisFastLaneAndFabricPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TravisFastLaneAndFabricPractice
+//
+//  Created by Yang, Yusheng on 3/29/18.
+//  Copyright © 2018 Yang, Yusheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
